1c554643e7c689a458bd2aa4ee314b93103895e5
Andrew Russo <russspruce@gmail.com>
2016-06-15 02:28:12 -0700

    Have fixed the form, but still having issues having it save due to where the saveAnswer3 is.

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -5,9 +5,7 @@ export default Ember.Component.extend({
     update(question, params) {
       this.sendAction('update', question, params);
     },
-    saveAnswer(answer, params) {
-      this.sendAction('saveAnswer', answer, params);
-    },
+    
     delete(question) {
       if (confirm('Are you sure you want to delete this question?')) {
         this.sendAction('deleteQuestion', question);
